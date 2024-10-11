a74766b4b16e63e527be079cde01002c2193f3c3
kurlerprudent <osarkodie@st.ug.edu.gh>
2024-10-11 19:40:33 +0000

    Update

--- app/admission/page.js
+++ app/admission/page.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import Coat from '@/Components/Coat';
 import Styles from './page.module.css';
-import Link from 'next/link';
 import SchoolForm from '@/Components/SchoolInput';
-
-
 
 const Page = () => {
    
-        const regions = [
-          'Greater Accra',
-          'Ashanti',
-          'Western',
-          'Eastern',
-          'Northern',
-        ];
-      
-        const schools = [
-          'School A',
-          'School B',
-          'School C',
-          'School D',
-        ];
+  const regions = [
+    'Greater Accra',
+    'Ashanti',
+    'Western',
+    'Eastern',
+    'Northern',
+  ];
+  
+  const schools = [
+
+'schools A',
+'School B',
+'School C',
+'School D',
+
+  ];
 
   return (
     <div>
@@ -29,8 +28,6 @@ const Page = () => {
         <div className={Styles.formContainer}>
             <SchoolForm regions={regions} schools={schools}/>
         </div>
-          
-       
         <div className={Styles.coatContainer}>
           <Coat />
         </div>
