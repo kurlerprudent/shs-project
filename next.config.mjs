81919b66a02e52d098a15bb7588d0d15414b58c0
Daquiver1 <Cabrokwa11@gmail.com>
2024-10-23 15:26:30 +0000

    feat: minor fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8008";
+    const apiUrl =
+      process.env.NEXT_PUBLIC_API_URL || "https://shsportal.fly.dev";
     console.log("apiUrl", apiUrl);
     return [
       {
